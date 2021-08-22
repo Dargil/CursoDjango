5f51fb2e219c46d18387b1da22047a186189bcd6
Dargil <jefferson.marin@correo.usa.edu.co>
2021-08-22 17:03:17 -0500

    Manage Url and templates

---
+++ prueba1/prueba1/urls.py
@@ -0,0 +1,31 @@
+"""prueba1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from prueba1.views import * # se debe importar la view
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('holamundo/', holamundo2),
+    #identificar un parametro por url
+    path('hola2/<nombre>', holamundo),
+        #identificar un parametro por url recibir un numero
+    path('recibonumero/<int:num>', recibonumero),
+
+
+    path('divi/<int:num1>/<int:num2>', division),
+    path('hola/<nombre>', holamundoPlantilla),
+    path('fecha/', fecha),
+]
---
+++ prueba1/prueba1/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+
+# Lectura directa del archivo
+def holamundoPlantilla(request,nombre):
+    """Vista Hola Mundo"""
+    doc = open ("/home/jefferson/Documents/Curso Django/CursoDjangoProjects/prueba1/prueba1//templates/miprimeraplantilla.html")
+    plt=Template(doc.read())
+    doc.close()
+    ctx=Context()
+    documento=plt.render(ctx)
+    return HttpResponse(documento)
+
+
+# se crea el archvo
+# Contenido estatico
+# cualquier metodo debe recibir el parametro request
+def holamundo2(request):
+    """Vista Hola Mundo"""
+    return HttpResponse("Bienvenidos al curso de Django")
+
+# forma de enviar la variable http://127.0.0.1:8000/hola/pedro
+def holamundo(request,nombre):
+    """Vista Hola Mundo"""
+    texto="Bienvenido %s al curso de Django "% nombre
+    return HttpResponse(texto)
+
+def recibonumero(request,num):
+    """Vista para impresión de número"""
+    texto="En número recibido es: %s"% num
+    return HttpResponse(texto)
+
+#contenido dinamico
+# cualquier metodo debe recibir el parametro request
+def fecha(request):
+    """ Vista para imprimir la fecha"""
+    hoy=datetime.datetime.now()
+    return HttpResponse(hoy)
+
+# Cómo enviar múltiples parámetros
+def division(request,num1,num2):
+    """ Vista para dividir dos números"""
+    divi=num1/num2
+    return HttpResponse("El resultado de la división es: %s" % divi)
