29658909dd66dace9438513b81eb4dd2494ee54a
Dargil <jefferson.marin@correo.usa.edu.co>
2021-08-22 19:17:50 -0500

    3. unidades incrustadas p1

--- prueba1/prueba1/settings.py
+++ prueba1/prueba1/settings.py
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'prueba1.urls'
-
+#DIRS': [os.path.join(BASE_DIR,'templates')],
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'prueba1/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS= [     
+    os.path.join(BASE_DIR,'static')
+]
 
 STATIC_URL = '/static/'
 
