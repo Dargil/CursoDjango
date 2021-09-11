02c742e94efe0b588294173e9b4ae8a1323c9ede
Dargil <jefferson.marin@correo.usa.edu.co>
2021-09-11 16:59:16 -0500

    4. Modelo en django 1

--- RentCar/Rentas/models.py
+++ RentCar/Rentas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Clientes(models.Model):
+    nombres=models.CharField(max_length=20)
+    apellidos=models.CharField(max_length=40)
+    fechaNaci=models.DateField()
+    cedula=models.IntegerField()
+    correo=models.EmailField()
+    telefono=models.IntegerField()
+
+    def __str__(self):
+        return 'Cliente %$ %$, fecha Nacimiento: %$, correo: %$' %(self.nombres,self.apellidos,self.fechaNaci,self.correo)
+
