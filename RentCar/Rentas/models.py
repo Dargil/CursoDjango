a92370e195dcedcda42638761926355d3f868441
Dargil <jefferson.marin@correo.usa.edu.co>
2021-09-27 20:01:08 -0500

    4. Relaciones en modelos

--- RentCar/Rentas/models.py
+++ RentCar/Rentas/models.py
@@ -21,3 +21,26 @@ class Autos(models.Model):
     valor_renta = models.IntegerField()
     def __str__(self):
         return 'Auto de tipo: %s, con placa: %s, marca: %s , modelo: %s' %(self.tipo,self.placa,self.marca,self.modelo)
+
+class Rentas(models.Model):
+    fecha_renta = models.DateField()
+    duracion_renta = models.IntegerField()
+    fecha_salida = models.DateField()
+    fecha_entrega = models.DateField()
+
+    #Relacion 1 a muchos
+
+    autos = models.ForeignKey(Autos, on_delete=models.CASCADE)
+    clientes = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    
+    #https://docs.djangoproject.com/en/3.2/topics/db/examples/many_to_one/
+    #https://docs.djangoproject.com/en/3.2/ref/models/fields/#foreignkey 
+
+    # Relacion 1 a 1
+    #capital= models.OneToOneField(Pais,on_delete=models.CASCADE)
+    #https://docs.djangoproject.com/en/3.2/topics/db/examples/many_to_one/
+
+    
+    def __str__(self):
+        return 'Auto de tipo: %s, con placa: %s, marca: %s , modelo: %s' %(self.tipo,self.placa,self.marca,self.modelo)
+
