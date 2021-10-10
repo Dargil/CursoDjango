26bb1c8fe924f59c2904ba960727209338a8b9d6
Dargil <jefferson.marin@correo.usa.edu.co>
2021-10-09 21:28:21 -0500

    parte 7 y 8

--- arena_simulacion/tiendavirtual/tiendavirtualapp/views.py
+++ arena_simulacion/tiendavirtual/tiendavirtualapp/views.py
@@ -1,10 +1,19 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template import Template, Context,loader
+from tiendavirtualapp.models import Categorias,Clientes,Productos,ProductosPedido,Inventario,Calificacion
 # Create your views here.
 
 def home(request):
-    return render(request,"base_template.html")
+    # Obtener inicialmente las categorias
+    all_cat = Categorias.objects.all().order_by('id')
+    # si pongo un signo menos lo ordena de forma descendente
+    latest_prod = Productos.objects.all().order_by('-id')[:6]
+    # Ordenar los productos por calificacion 
+    top_prod = Calificacion.objects.all().order_by('-calificacion')[:6]
+    latest_review = Calificacion.objects.all().order_by('-id')[:6]
+    return render(request,"home.html",{"all_cat":all_cat,"latest_prod":latest_prod,"top_prod":top_prod,"latest_review":latest_review})
+
 def lista_productos(request):
     return HttpResponse("lista productos")
 def producto_detalles(request,product_id):
