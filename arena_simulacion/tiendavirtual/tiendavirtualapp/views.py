beb6ac49bcf54647aa17174ca6a0ba688723181f
Dargil <jefferson.marin@correo.usa.edu.co>
2021-10-09 19:57:42 -0500

    parte 6

--- arena_simulacion/tiendavirtual/tiendavirtualapp/views.py
+++ arena_simulacion/tiendavirtual/tiendavirtualapp/views.py
@@ -1,9 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.template import Template, Context,loader
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Este es el home")
+    return render(request,"base_template.html")
 def lista_productos(request):
     return HttpResponse("lista productos")
 def producto_detalles(request,product_id):
